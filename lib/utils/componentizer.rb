a02daf20b54e4768ae418be13f5ec31d974b5035
Joseph Pearson <joseph@inventivelabs.com.au>
2010-08-03 14:28:24 +1000

    Componentizer utility and tests.

---
+++ lib/utils/componentizer.rb
@@ -0,0 +1,110 @@
+class Peregrin::Componentizer
+
+  attr_reader :component_xpaths
+
+
+  def initialize(doc)
+    @document = doc
+    @component_xpaths = []
+  end
+
+
+  # Build a list of xpaths for nodes that can be turned into standalone
+  # components.
+  #
+  def process(from)
+    @component_xpaths = []
+    walk(from)
+    @component_xpaths.reject! { |xpath| emptied?(xpath) }
+  end
+
+
+  # Creates a new document with the same root and head nodes, but with
+  # a body that just contains the nodes at the given xpath.
+  #
+  def generate_component(xpath)
+    raise "Not a component: #{xpath}"  unless @component_xpaths.include?(xpath)
+
+    # Clean up the "shell" document.
+    @shell_document ||= @document.root.dup
+    bdy = @shell_document.at_css('body')
+    bdy.children.remove
+
+    # Find the node we're going to copy into the shell document.
+    # Create a deep clone of it. Remove any children of it that are
+    # componentizable in their own right.
+    node = @document.at_xpath(xpath)
+    ndup = node.dup
+    node.children.collect { |ch|
+      next  unless component_xpaths.include?(ch.path)
+      dpath = ch.path.sub(/^#{Regexp.escape(node.path)}/, ndup.path)
+      ndup.children.detect { |dch| dch.path == dpath }
+    }.compact.each { |ch|
+      ch.unlink
+    }
+
+    # Append the node to the body of the shell (or replace the body, if
+    # the node is a body itself).
+    if xpath == "/html/body"
+      bdy.replace(ndup)
+    else
+      bdy.add_child(ndup)
+    end
+
+    @shell_document
+  end
+
+
+  # Writes the componentizable node at the given xpath to the given
+  # filesystem path.
+  #
+  # If you provide a block, you get the new document object,
+  # and you are expected to return the string containing its HTML form --
+  # in this way you can tweak the HTML output. Default is simply: doc.to_html
+  #
+  def write_component(xpath, path, &blk)
+    new_doc = generate_component(xpath)
+    out = block_given? ? blk.call(new_doc) : new_doc.to_html
+    File.open(path, 'w') { |f| f.write(out) }
+    out
+  end
+
+
+  protected
+
+    # The recursive method for walking the tree - checks if the current node
+    # is a component, then checks each child of the current node.
+    #
+    def walk(node)
+      return  unless componentizable?(node)
+      @component_xpaths.push(node.path)
+      node.children.each { |c| walk(c) }
+    end
+
+
+    # True if the node meets the criteria for being componentizable:
+    #   1) Is a body or article element (or a div.article)?
+    #   2) Are all subsequent siblings also componentizable?
+    #
+    def componentizable?(node)
+      begin
+        return false  unless (
+          %w[body article].include?(node.name.downcase) ||
+          (node.name.downcase == "div" && node['class'].match(/\barticle\b/))
+        )
+      end while node = node.next
+      true
+    end
+
+
+    # True if all children are either componentizable or blank text nodes.
+    #
+    def emptied?(xpath)
+      node = @document.at_xpath(xpath)
+      node.children.all? { |ch|
+        @component_xpaths.include?(ch.path) ||
+        (ch.text? && ch.content.strip.empty?)
+      }
+    end
+
+end
---
+++ test/utils/componentizer_test.rb
@@ -0,0 +1,79 @@
+require 'test/helper'
+
+class Peregrin::Tests::ComponentizerTest < Test::Unit::TestCase
+
+  def test_processing
+    cz = process_fixture("components1.html")
+    assert_equal(
+      [
+        '/html/body',
+        '/html/body/article[1]',
+        '/html/body/article[1]/article[1]',
+        '/html/body/article[1]/article[2]',
+        '/html/body/article[2]'
+      ],
+      cz.component_xpaths
+    )
+  end
+
+
+  def test_processing_where_body_should_be_empty
+    cz = process_fixture("components2.html")
+    assert_equal(
+      [
+        '/html/body/article[1]',
+        '/html/body/article[1]/article[1]',
+        '/html/body/article[1]/article[2]',
+        '/html/body/article[2]',
+        '/html/body/article[2]/article'
+      ],
+      cz.component_xpaths
+    )
+  end
+
+
+  def test_generate_component
+    cz = process_fixture("components1.html")
+    assert_equal(
+      whitewash(
+        "<html><head><title>Components test 1</title></head><body>" +
+        "<article><h2>B</h2></article>" +
+        "</body></html>"
+      ),
+      whitewash(cz.generate_component('/html/body/article[1]').to_html)
+    )
+  end
+
+
+  def test_write_component
+    cz = process_fixture("components1.html")
+    tmp_path = "test/fixtures/componentizer/tmp.html"
+    cz.write_component("/html/body", tmp_path) { |doc| doc.to_xhtml }
+    assert_equal(
+      whitewash(
+        "<html xmlns=\"http://www.w3.org/1999/xhtml\">" +
+        "<head><title>Components test 1</title></head>" +
+        "<body><h1>A</h1></body></html>"
+      ),
+      whitewash(IO.read(tmp_path))
+    )
+  ensure
+    File.unlink(tmp_path)
+  end
+
+
+  protected
+
+    def process_fixture(filename)
+      fx = File.new("test/fixtures/componentizer/#{filename}")
+      doc = Nokogiri::HTML::Document.parse(fx)
+      cz = Peregrin::Componentizer.new(doc)
+      cz.process(doc.at_xpath('/html/body'))
+      cz
+    end
+
+    def whitewash(str)
+      str.gsub(/\s+/,'')
+    end
+
+end
